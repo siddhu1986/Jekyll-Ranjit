43598359687e9b00a9d911382b431a5d612c082c
siddhu1986 <siddata1986@gmail.com>
2022-05-08 01:45:53 +0530

    Cleaning
    Cleaning

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-/*
-$(function(){
-  $(window).scroll(function(){
-    var winTop = $(window).scrollTop();
-    if(winTop >= 30){
-      $("body").addClass("sticky-header");
-    }else{
-      $("body").removeClass("sticky-header");
-    }//if-else
-  });//win func.
-});//ready func
-*/
-
-
-
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
@@ -20,21 +5,20 @@ var sticky = header.offsetTop;
 
 function myFunction() {
   $("#mnu").hide();
-  if (window.pageYOffset > sticky) {
-    //header.classList.add("sticky");
+  if (window.pageYOffset > sticky) 
+  {
     header.classList.add("stuck");
     $(".logo_white").hide();
     $(".logo_blue").show();
-  } else {
-    //header.classList.remove("sticky");
+  }
+  else
+  {
     header.classList.remove("stuck");
     $(".logo_white").show();
     $(".logo_blue").hide();
   }
 }
 
-var __mnuHovered = -1;
-var __mnuHoveredSI = null;
 $(document).ready(function()
 {
   $("#myHeader a").each(function()
@@ -42,7 +26,6 @@ $(document).ready(function()
     if($("#myHeader.stuck").length>0) return;
     $(this).on("mouseover",function()
     {
-      //alert();
       var alias = $(this).attr("data-name");
       if(!alias)
       {
@@ -52,35 +35,7 @@ $(document).ready(function()
       $("#mnu").show();
       $("#mnu").find(".mnu-inner").hide();
       $("#mnu").find(".mnu-"+alias).show();
-      return;
-      $("#mnu").on("mouseover",function()
-      {
-        __mnuHovered = true;
-        if(__mnuHoveredSI)
-        {
-          clearTimeout(__mnuHoveredSI);
-          __mnuHoveredSI = null
-
-        }
-        __mnuHoveredSI = setTimeout(function()
-        {
-          __mnuHovered = false;
-        },2000);
-      });
-      $("body").on("mousemove",function()
-      {
-        console.log("mm");
-        if(__mnuHovered!=-1 && __mnuHovered==false)
-        {
-          $("#mnu").hide();
-          $("body").off("mousemove");
-          __mnuHovered = -1;
-        }
-      });
-      /*$(".topbanner").off("mouseout").on("mouseout",function()
-      {
-        //$("#mnu").hide();
-      });*/
+      
     });
   });
 });
